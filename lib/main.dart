4f2c54fe2970fc0e71766330a4e547af3ae70d1b
David Jo <war2290@gmail.com>
2024-04-11 16:36:49 +0900

    init: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_profile/theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ProfileDrawer(),
+      // AppBar(),
+      body: Column(
+        children: [
+          // ProfileHeader(),
+          // ProfileCountInfo(),
+          // ProfileButtons(),
+          // ProfileTab(),
+        ],
+      ),
+    );
   }
 }
