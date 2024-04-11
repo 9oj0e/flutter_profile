14df9003b6578184b07c96bdcb4db1f5e0ec5f9a
David Jo <war2290@gmail.com>
2024-04-11 17:33:42 +0900

    init: profile_buttons

--- lib/components/profile_buttons.dart
+++ lib/components/profile_buttons.dart
@@ -14,7 +14,46 @@ class ProfileButtons extends StatelessWidget {
     );
   }
 
-  SizedBox _buildFollowButton() => SizedBox();
+  Widget _buildFollowButton() {
+    return InkWell(
+      onTap: () {
+        print("Follow 버튼 클릭됨");
+      },
+      child: Container(
+        alignment: Alignment.center, // 내부 Text 가운데 정렬
+        width: 150,
+        height: 45,
+        child: Text(
+          "Follow",
+          style: TextStyle(color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+        ),
+      ),
+    );
+  }
 
-  SizedBox _buildMessageButton() => SizedBox();
+  Widget _buildMessageButton() {
+    return InkWell(
+      onTap: () {
+        print("Message 버튼 클릭됨");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        child: Text(
+          "Message",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(), // 테두리 선 추가
+        ),
+      ),
+    );
+  }
 }
